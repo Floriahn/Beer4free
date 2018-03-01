474a8cfff5ecb3fbaf02c7a16c4219b6afffe393
Floriahn <32749310+Floriahn@users.noreply.github.com>
2018-03-01 20:18:42 +0100

    gastui
    insert gast

--- TestDrivenHotelDB/src/InsertGast.java
+++ TestDrivenHotelDB/src/InsertGast.java
@@ -4,17 +4,21 @@ import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.awt.event.ActionEvent;
 
 public class InsertGast {
 
 	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
+	private JTextField nachnametxt;
+	private JTextField nametxt;
+	private JTextField GDtxt;
+	private JTextField landtxt;
+	private JTextField telefontxt;
+	private JTextField hntxt;
+	private JTextField plztxt;
 
 	/**
 	 * Launch the application.
@@ -45,14 +49,14 @@ public class InsertGast {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setResizable(false);
-		frame.setBounds(100, 100, 399, 526);
+		frame.setBounds(100, 100, 399, 447);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		textField = new JTextField();
-		textField.setBounds(44, 84, 86, 20);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		nachnametxt = new JTextField();
+		nachnametxt.setBounds(44, 84, 86, 20);
+		frame.getContentPane().add(nachnametxt);
+		nachnametxt.setColumns(10);
 		
 		JLabel lblNeuerGast = new JLabel("Neuer Gast:");
 		lblNeuerGast.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -60,61 +64,83 @@ public class InsertGast {
 		frame.getContentPane().add(lblNeuerGast);
 		
 		JLabel lblNachname = new JLabel("Nachname*");
-		lblNachname.setBounds(44, 53, 65, 20);
+		lblNachname.setBounds(44, 53, 96, 20);
 		frame.getContentPane().add(lblNachname);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(251, 84, 86, 20);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
+		nametxt = new JTextField();
+		nametxt.setBounds(251, 84, 86, 20);
+		frame.getContentPane().add(nametxt);
+		nametxt.setColumns(10);
 		
 		JLabel lblName = new JLabel("Name");
 		lblName.setBounds(249, 56, 65, 17);
 		frame.getContentPane().add(lblName);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(44, 160, 86, 20);
-		frame.getContentPane().add(textField_2);
+		GDtxt = new JTextField();
+		GDtxt.setColumns(10);
+		GDtxt.setBounds(44, 160, 86, 20);
+		frame.getContentPane().add(GDtxt);
 		
 		JLabel lblGeburtsdatumyyyymmdd = new JLabel("Geburtsdatum (YYYY-MM-DD)*");
-		lblGeburtsdatumyyyymmdd.setBounds(44, 132, 149, 20);
+		lblGeburtsdatumyyyymmdd.setBounds(44, 132, 195, 20);
 		frame.getContentPane().add(lblGeburtsdatumyyyymmdd);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(251, 160, 86, 20);
-		frame.getContentPane().add(textField_3);
-		textField_3.setColumns(10);
+		landtxt = new JTextField();
+		landtxt.setBounds(251, 160, 86, 20);
+		frame.getContentPane().add(landtxt);
+		landtxt.setColumns(10);
 		
 		JLabel lblLand = new JLabel("Land*");
 		lblLand.setBounds(249, 135, 65, 17);
 		frame.getContentPane().add(lblLand);
 		
-		textField_4 = new JTextField();
-		textField_4.setBounds(44, 235, 86, 20);
-		frame.getContentPane().add(textField_4);
-		textField_4.setColumns(10);
+		telefontxt = new JTextField();
+		telefontxt.setBounds(44, 235, 86, 20);
+		frame.getContentPane().add(telefontxt);
+		telefontxt.setColumns(10);
 		
 		JLabel lblTelefon = new JLabel("Telefon");
 		lblTelefon.setBounds(44, 207, 65, 17);
 		frame.getContentPane().add(lblTelefon);
 		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		textField_5.setBounds(251, 235, 86, 20);
-		frame.getContentPane().add(textField_5);
+		hntxt = new JTextField();
+		hntxt.setColumns(10);
+		hntxt.setBounds(251, 235, 86, 20);
+		frame.getContentPane().add(hntxt);
 		
 		JLabel lblHausnummer = new JLabel("Hausnummer*");
 		lblHausnummer.setBounds(249, 208, 104, 17);
 		frame.getContentPane().add(lblHausnummer);
 		
-		textField_6 = new JTextField();
-		textField_6.setColumns(10);
-		textField_6.setBounds(147, 307, 86, 20);
-		frame.getContentPane().add(textField_6);
+		plztxt = new JTextField();
+		plztxt.setColumns(10);
+		plztxt.setBounds(147, 307, 86, 20);
+		frame.getContentPane().add(plztxt);
 		
 		JLabel lblPlz = new JLabel("PLZ*");
 		lblPlz.setBounds(147, 279, 65, 17);
 		frame.getContentPane().add(lblPlz);
+		
+		JButton btnNewButton = new JButton("Speichern");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Gast g = new Gast(nachnametxt.getText(), GDtxt.getText(), plztxt.getText(), landtxt.getText(), hntxt.getText(), telefontxt.getText(), nametxt.getText());
+				try {
+					DBManager manager = DBManager.getInstance();
+					manager.insertGast(g);
+				} catch (ClassNotFoundException | SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				nachnametxt.setText(""); GDtxt.setText("");plztxt.setText("");landtxt.setText("");hntxt.setText("");telefontxt.setText("");nametxt.setText("");
+			}
+		});
+		
+		btnNewButton.setBounds(140, 359, 104, 31);
+		frame.getContentPane().add(btnNewButton);
+		
+		JLabel lblMuss = new JLabel("* = muss angegeben werden");
+		lblMuss.setBounds(10, 393, 256, 14);
+		frame.getContentPane().add(lblMuss);
 	}
 }
